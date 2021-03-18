b57c6832a973db39b450644f88f1a29cb323f0b0
Morag Scrimgeour <8700885+mscrim@users.noreply.github.com>
2021-03-18 22:45:46 +0000

    Edits

--- strategies.py
+++ strategies.py
@@ -1,11 +1,11 @@
 from datetime import timedelta
 
 def check_weekday(start_date):
-	""" Check that start_date is a weekday
+    """ Check that start_date is a weekday
 
-	start_date: pd.Timestamp
+    start_date: pd.Timestamp
 
-	"""
+    """
     weekday_no = start_date.weekday()
     if weekday_no == 5: # Saturday
         start_date = start_date + timedelta(days=2)
@@ -14,7 +14,7 @@ def check_weekday(start_date):
     return start_date
 
 def buy_and_hold(d, start_date, end_date):
-	"""
+    """
     Buy and Hold strategy
 
     Inputs
@@ -22,17 +22,17 @@ def buy_and_hold(d, start_date, end_date):
     d : pd.Series
         Daily prices for a single stock ticker
     start_date : pd.Timestamp
- 		Start date for the strategy
-	end_date : pd.Timestamp
-		End date for the strategy
+        Start date for the strategy
+    end_date : pd.Timestamp
+        End date for the strategy
 
     Returns
     -------
     factor : float
         Factor by which initial investment changes
     perc_change : float
-    	Percentage by which initial investment changes
-	"""
+        Percentage by which initial investment changes
+    """
     start_date = check_weekday(start_date)
     init_price = d.loc[start_date][0]
     final_price = d.loc[end_date][0]
